39157decdcbf3160c399de5da2677d2a16036583
binhoul <saintli86@163.com>
2014-08-15 10:48:37 +0800

    Update test_methods.py

--- scripts/test_methods.py
+++ scripts/test_methods.py
@@ -3,7 +3,7 @@ import sys
 import MySQLdb
 
 def do_all():
-    db = MySQLdb.connect('10.192.0.5','root','fengmao','cp')
+    db = MySQLdb.connect('10.192.0.5','root','fexgmxo','cp')
     cursor = db.cursor()
     sql = 'select phase from bingo where index_id > 5'
     cursor.execute(sql)
